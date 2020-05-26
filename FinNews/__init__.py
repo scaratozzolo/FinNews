180882b9d631ac1beb2e5c87cbe9d5fc3bca79eb
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-05-26 13:27:22 -0400

    bug fix

--- FinNews/__init__.py
+++ FinNews/__init__.py
@@ -1,7 +1,8 @@
+from FinNews.feed import Feed
 from FinNews.sources import *
 
 name = "FinNews"
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
-__all__ = ['Feed', 'CNBC', 'SeekingAlpha', 'Investing', 'WSJ', 'Yahoo', 'FT']
+__all__ = ['Feed', 'CNBC', 'SeekingAlpha', 'Investing', 'WSJ', 'Yahoo', 'FT', 'Fortune']
